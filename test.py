d7971febf0c0e3cb0839a72c0377264e1df4847a
ranoodles <115419765+ranoodles@users.noreply.github.com>
2024-07-04 11:00:21 -0700

    basicdockingcode

--- test.py
+++ test.py
@@ -1,15 +1,17 @@
 import rdkit.Chem as Chem
 from dockstring import load_target
-from utils import pdb_to_smiles
+from utilities import pdb_to_smiles
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 smiles = pdb_to_smiles('propionate.pdb')
-
-target = load_target('DRD')
+target = load_target('or51e2', os.getenv('TEMP_DIR'), os.getenv('TARGET_DIR'))
 score, aux = target.dock(smiles)
-
 print(aux)
 
 os.environ['PATH'] += ':/Applications/PyMOL.app/Contents/bin'
 
 target.view([aux['ligand']])
+
